cc27ca3d8834863ff5513600cd99e41c9488ae08
Sloane Simmons <sloanes.k@gmail.com>
2013-07-10 08:29:53 +0000

    Buffer changes

--- web.js
+++ web.js
@@ -1,13 +1,11 @@
 var express = require('express');
-var buffer = new Buffer(500);
+var buffer = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var path = require('path');
-var fd = fs.openSync(path.join(process.cwd(), 'index.html'),'r');
-var length = fs.readSync(fd,buffer,500);
+
 app.get('/', function(request, response) {
-  response.send(buffer.toString('ascii'));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
