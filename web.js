b9f2ce9111b564a7d055bf6345715343425c828f
Sloane Simmons <sloanes.k@gmail.com>
2013-07-10 08:14:39 +0000

    Buffer testing

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var buffer = new Buffer(50);
+var buffer = new Buffer(500);
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
 var path = require('path');
 var fd = fs.openSync(path.join(process.cwd(), 'index.html'),'r');
-var length = fs.readSync(fd,buffer);
+var length = fs.readSync(fd,buffer,500);
 app.get('/', function(request, response) {
   response.send(buffer.toString('ascii',0,length));
 });
