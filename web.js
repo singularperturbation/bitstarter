37ad90ebacd126f3b475aac0c2e06f50a9099573
Sloane Simmons <sloanes.k@gmail.com>
2013-07-24 22:17:10 -0500

    HW4

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+var app = express();
+app.use(express.logger());
 var fs = require('fs');
 var buffer = fs.readFileSync('index.html');
 
@@ -7,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
